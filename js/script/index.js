b434774b3c743fc213d2deba7acfc55ad2a58a0f
DESKTOP-M7FFJPM\Lucas TAMOMO <tamomo.lucas@gmail.com>
2023-05-29 20:49:56 +0100

    first commit

--- js/script/index.js
+++ js/script/index.js
@@ -1,3 +1,38 @@
+// Tendency
+$(document).ready(function(){
+  $('.tendency-slick').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+      responsive: [
+        {
+          breakpoint: 1240,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 765,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+          }
+        }
+      ]
+    });
+  });  
+
+// Review
 $(document).ready(function(){
   $('.review-slick').slick({
     arrows: false,
@@ -30,37 +65,3 @@ $(document).ready(function(){
       ]
     });
   });
-
-
-$(document).ready(function(){
-  $('.v-slick').slick({
-    arrows: true,
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 6000,
-      responsive: [
-        {
-          breakpoint: 1240,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            arrows: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 765,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true
-          }
-        }
-      ]
-    });
-  });  
